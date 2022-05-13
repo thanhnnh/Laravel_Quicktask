36541f082f8bfe9609080946167eb9c87d6e28f0
ThanhDC <duongcongthanh97@gmail.com>
2022-05-13 13:22:31 +0700

    middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('check.admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
